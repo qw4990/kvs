8b4eff26c24cb00f7f341b98b171911ba0873f97
qw4990 <qw4990@163.com>
2019-06-08 15:43:35 +0800

    fill lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+pub struct KvStore {}
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        panic!();
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        panic!();
+    }
+
+    pub fn get(&mut self, key: String) -> Option<String> {
+        panic!();
+    }
+
+    pub fn remove(&mut self, key: String) {
+        panic!();
+    }
+}
