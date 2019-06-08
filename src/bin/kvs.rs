7e0df629785d13fe11afb0d3d183fa5b4c940e7f
qw4990 <qw4990@163.com>
2019-06-08 16:22:42 +0800

    make cli_version pass

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,8 +1,20 @@
 extern crate clap;
-use clap::{Arg, App, SubCommand};
+
+use std::process::exit;
+
+use clap::{App, Arg, SubCommand};
 
 fn main() {
-    let matches = App::new("kvs").arg(Arg::with_name("cmd"));
+    let matches = App::new("kvs").version(env!("CARGO_PKG_VERSION"))
+        .arg(Arg::with_name("cmd").index(1).required(false))
+        .arg(Arg::with_name("key").index(2).required(false))
+        .arg(Arg::with_name("val").index(3).required(false))
+        .arg(Arg::with_name("V").short("V"))
+        .get_matches();
 
-    println!("Hello, world!");
+    if matches.occurrences_of("V") > 0 {
+        println!("Version {}", env!("CARGO_PKG_VERSION"));
+        exit(0);
+    }
+    exit(1);
 }
