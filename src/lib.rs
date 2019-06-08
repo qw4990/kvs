d8dd4c63e1e6470e772c23731db8b049d4a113bf
qw4990 <qw4990@163.com>
2019-06-08 16:54:44 +0800

    make all test pass

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,26 @@
-pub struct KvStore {}
+use std::collections::HashMap;
+
+pub struct KvStore {
+    m: HashMap<String, String>,
+}
 
 impl KvStore {
     pub fn new() -> KvStore {
-        panic!();
+        KvStore { m: HashMap::new() }
     }
 
     pub fn set(&mut self, key: String, value: String) {
-        panic!();
+        self.m.insert(key, value);
     }
 
     pub fn get(&mut self, key: String) -> Option<String> {
-        panic!();
+        match self.m.get(&key) {
+            None => None,
+            Some(val) => Some(val.to_owned()),
+        }
     }
 
     pub fn remove(&mut self, key: String) {
-        panic!();
+        self.m.remove(&key);
     }
 }
