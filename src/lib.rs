ad249a2e6e14f310ec0e1a2e7988f6a846c15aba
qw4990 <qw4990@163.com>
2019-06-09 14:36:40 +0800

    make it compile

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,24 @@
 use std::collections::HashMap;
+use std::io;
+use std::path::Path;
+use std::result;
+
+use failure::Fail;
 
 /// A KV store.
 pub struct KvStore {
     m: HashMap<String, String>,
 }
+
+/// Error type for kvs
+#[derive(Fail, Debug)]
+pub enum KvsError {
+    #[fail(display = "Unexpected command type")]
+    UnexpectedCommandType,
+}
+
+/// Result type for kvs
+pub type Result<T> = std::result::Result<T, KvsError>;
 
 impl KvStore {
     /// Creates a KvStore.
@@ -11,18 +26,24 @@ impl KvStore {
         KvStore { m: HashMap::new() }
     }
 
+    pub fn open(p: &Path) -> Result<KvStore> {
+        panic!("xxx");
+    }
+
     /// Set a new KV.
-    pub fn set(&mut self, key: String, value: String) {
+    pub fn set(&mut self, key: String, value: String) -> Result<()> {
         self.m.insert(key, value);
+        Ok(())
     }
 
     /// Get ...
-    pub fn get(&mut self, key: String) -> Option<String> {
-        self.m.get(&key).cloned()
+    pub fn get(&mut self, key: String) -> Result<Option<String>> {
+        Ok(self.m.get(&key).cloned())
     }
 
     /// Remove ...
-    pub fn remove(&mut self, key: String) {
+    pub fn remove(&mut self, key: String) -> Result<()> {
         self.m.remove(&key);
+        Ok(())
     }
 }
